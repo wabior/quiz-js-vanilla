513d5b8cb00cab63b82ac4da7e178ebe78378333
jaroslaw.wabich <jaroslaw.wabich@wfirma.pl>
2021-10-29 15:00:23 +0200

    kilka pytan

--- app/app.js
+++ app/app.js
@@ -1,12 +1,14 @@
 
 let questionIndex = 0;
-const questionArea = document.querySelector("main article");
+const questionArea = document.querySelector('main article');
 const buttons = document.querySelectorAll('.quiz-button');
+const glass = document.querySelector('.screen');
 
 function showQuestion(index){
     let currentQuestion = questions[index];
     questionArea.textContent = currentQuestion.question;
     showAnswers(currentQuestion);
+
 }
 
 function showAnswers(choices) {
@@ -15,6 +17,8 @@ function showAnswers(choices) {
         btn.addEventListener('click', function () {
             console.log(index);
             if (index == choices.correctAnswerIndex) {
+                animate1();
+                setTimeout(animate2,300);
                 questionIndex++;
                 showQuestion(questionIndex);
             } else {
@@ -24,4 +28,18 @@ function showAnswers(choices) {
     });
 }
 
+function animate1(){
+    glass.classList.add('nextQuestion');
+}
+
+function animate2(){
+    glass.classList.remove('nextQuestion');
+}
+
+// function wrongAnswer(){
+//     glass.classList.add('wrongAnswer');
+//     setTimeout(() => glass.classList.remove('wrongAnswer'))
+// }
+
 showQuestion(questionIndex);
+
--- app/questions.js
+++ app/questions.js
@@ -1,9 +1,9 @@
 questions =
     [
         {
-          "question": "Które wyrażenie nie należy do proaktywnych:",
-          "answerChoices": ["Mogę", "Zobaczmy, co można z tym zrobić", "To nie moja wina", "Zastanówmy się jakie mamy opcje"],
-          "correctAnswerIndex": 2,
+            "question": "Które wyrażenie nie należy do proaktywnych:",
+            "answerChoices": ["Mogę", "Zobaczmy, co można z tym zrobić", "To nie moja wina", "Zastanówmy się jakie mamy opcje"],
+            "correctAnswerIndex": 2,
         },
         {
             "question": "Które elementy umieszczamy w pierwszej ćwiartce:",
@@ -15,4 +15,39 @@ questions =
             "answerChoices": ["1 + 1 = 2", "1 + 1 = 3", "2 != 5", "n!"],
             "correctAnswerIndex": 1,
         },
+        // {
+        //     "question": "Która odpowiedź zawiera poprawną kolejność proaktywnego działania?",
+        //     "answerChoices": ["REFLEKSJA-WYBÓR->DZIAŁANIE->BODZIEC", "BODZIEC->DZIAŁANIE->REFLEKSJA-WYBÓR", "BODZIEC->REFLEKSJA-WYBÓR->DZIAŁANIE", "REFLEKSJA-WYBÓR->BODZIEC->DZIAŁANIE"],
+        //     "correctAnswerIndex": 2,
+        // },
+        {
+            "question": "Reaktywne działanie jest oparte na...",
+            "answerChoices": ["uczuciach", "wartościach", "emocjach", "potrzebach"],
+            "correctAnswerIndex": 0,
+        },
+        {
+            "question": "Wybierz zestaw słów z języka proaktywnego.",
+            "answerChoices": ["nie mogę, muszę, z tym nie da się nic zrobić", "zrobię to, muszę, wybieram", "to nie moja wina, mogę, wybieram", "mogę, wybieram, zrobię to"],
+            "correctAnswerIndex": 3,
+        },
+        {
+            "question": "Nawykiem poszukiwania wzajemnych korzyści jest?",
+            "answerChoices": ["przegrana-wygrana", "przegrana-przegrana", "wygrana-wygrana", "wygrana-przegrana"],
+            "correctAnswerIndex":  2,
+        },
+        {
+            "question": "Nawyk komunikacji empatycznej oparty jest na zasadach...",
+            "answerChoices": ["szacunku, wzajemnego zrozumienia,empatii i odwagi", "korzyściach, wartościach i odwadze", "szacunku, emocjach, potrzebach", "potrzebach, korzyściach, uczuciach"],
+            "correctAnswerIndex": 0,
+        },
+        {
+            "question": "Jak brzmi Nawyk pierwszy?",
+            "answerChoices": ["Zaczynaj z wizją końca", "Bądź proaktywny", "Twórz synergię", "Myśl w kategoriach wygrana-wygrana"],
+            "correctAnswerIndex": 1,
+        },
+        {
+            "question": "Który z wymienionych nawyków NIE jest jednym z 7 nawyków skutecznego działania?",
+            "answerChoices": ["Bądź proaktywny", "Rób najpierw to, co najważniejsze", "Ostrz piłę", "Myśl w kategoriach wygrana-przegrana"],
+            "correctAnswerIndex": 3,
+        }
     ]
