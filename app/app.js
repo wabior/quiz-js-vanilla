26a8ff5617f439422e2ad7b69b4f15140c55a542
wabior <wabior@gmail.com>
2021-11-06 11:09:02 +0100

    refactor

--- app/app.js
+++ app/app.js
@@ -1,39 +1,51 @@
-let questionIndex = 0;
-let correctAnswer = null;
-let score = {'correct': 0, 'wrong': 0};
 const questionArea = document.querySelector('main article');
 const buttons = document.querySelectorAll('.quiz-button');
 const glass = document.querySelector('.screen');
+let questionIndex = 0;
+let correctAnswer = null;
+let score = {'correct': 0, 'wrong': 0};
 questions = questions.sort((a, b) => 0.5 - Math.random());
+
+function onClick(btn, idx){
+    if (idx === correctAnswer){
+
+        ++score.correct;
+
+        if (questionIndex < questions.length - 1){
+
+            glass.classList.add('nextQuestion');
+            questionArea.classList.add('d-none');
+            correctAnswer = showQuestion(++questionIndex);
+            buttons.forEach((btn) => btn.classList.add('d-none'))
+            setTimeout(function(){
+                glass.classList.remove('nextQuestion');
+                buttons.forEach((btn) => btn.classList.remove('d-none', 'hasactive', 'focus'));
+                document.activeElement = null;
+                questionArea.classList.remove('d-none');
+            }, 600);
+            
+        } else {
+            glass.classList.add('nextQuestion');
+            questionArea.classList.add('d-none');
+            setTimeout(function(){
+                glass.classList.remove('nextQuestion');
+                document.activeElement = null;
+                questionArea.classList.remove('d-none');
+            }, 600);
+            
+            questionArea.innerHTML="<p>Gratulacje.</p><br><hr><br><p>Błędnych odpowiedzi: " + score.wrong + "</p>";
+            buttons.forEach((btn) => btn.classList.add('d-none'));
+        }
+    } else {
+        btn.classList.add('wrongAnswer');
+        ++score.wrong;
+    }
+}
 
 function buttonsActions(){
     buttons.forEach(function(btn, idx){
-        btn.addEventListener('click', function(){
-
-            if (idx === correctAnswer){
-                if (questionIndex < questions.length - 1){
-
-                    ++score.correct;
-                    glass.classList.add('nextQuestion');
-                    questionArea.classList.add('d-none');
-                    correctAnswer = showQuestion(++questionIndex);
-                    buttons.forEach((btn) => btn.classList.add('d-none'))
-                    setTimeout(function(){
-                        glass.classList.remove('nextQuestion');
-                        buttons.forEach((btn) => btn.classList.remove('d-none', 'hasactive', 'focus'));
-                        document.activeElement = null;
-                        questionArea.classList.remove('d-none');
-                    }, 600);
-                    
-                } else {
-                    ++score.correct;
-                    questionArea.innerHTML="<p>Gratulacje.</p><br><hr><br><p>Błędnych odpowiedzi: " + score.wrong + "</p>";
-                    buttons.forEach((btn) => btn.classList.add('d-none'));
-                }
-            } else {
-                btn.classList.add('wrongAnswer');
-                ++score.wrong;
-            }
+        btn.addEventListener('click', ()=>{
+            onClick(btn, idx);
         });
     });
 }
